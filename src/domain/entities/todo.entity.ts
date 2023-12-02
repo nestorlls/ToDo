be1405dccdce88d2ca87421c53fb63db4ad845b3
Paul Llanque <llanquepol@gmail.com>
2023-12-02 01:18:08 -0500

    [ADD] transform obj to entity

--- src/domain/entities/todo.entity.ts
+++ src/domain/entities/todo.entity.ts
@@ -8,4 +8,19 @@ export class TodoEnity {
   get isCompleted() {
     return !!this.completedAt;
   }
+
+  public static fromObject(obj: { [key: string]: any }): TodoEnity {
+    const { id, text, completedAt } = obj;
+    if (!id) throw 'Id is required';
+    if (!text) throw 'Text is required';
+    let newCompletedAt;
+    if (completedAt) {
+      newCompletedAt = new Date(completedAt);
+      if (isNaN(newCompletedAt.getTime())) {
+        throw 'Property "CompletedAt" must be a valid date';
+      }
+    }
+
+    return new TodoEnity(id, text, newCompletedAt);
+  }
 }
