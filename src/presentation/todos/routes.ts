89dd8b8ef166c0f8e94bdab113a6617927c0f342
Paul Llanque <llanquepol@gmail.com>
2023-12-02 03:01:12 -0500

    [ADD] middleware parse id

--- src/presentation/todos/routes.ts
+++ src/presentation/todos/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { TodosController } from './controller';
 import { TodoDataSourceImpl, TodoRepositoryImpl } from '../../infrastructure';
+import { parseId } from '../../middlewares/parce-id.middleware';
 
 export class TodoRoutes {
   static get routes(): Router {
@@ -10,10 +11,10 @@ export class TodoRoutes {
     const todosController = new TodosController(repository);
 
     router.get('/', todosController.getAll);
-    router.get('/:id', todosController.getById);
+    router.get('/:id', [parseId], todosController.getById);
     router.post('/', todosController.create);
-    router.put('/:id', todosController.update);
-    router.delete('/:id', todosController.deleteTodo);
+    router.put('/:id', [parseId], todosController.update);
+    router.delete('/:id', [parseId], todosController.deleteTodo);
 
     return router;
   }
